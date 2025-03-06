3cef792f1ff1bd67247a641c3658abc479b9eae9
estban000 <marcellbanol@hotmail.com>
2025-03-06 13:21:52 +0100

    menu pause

--- Entrainement/Assets/Script/CurrentScreenManager.cs
+++ Entrainement/Assets/Script/CurrentScreenManager.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class CurrentScreenManager : MonoBehaviour
 {
     public GameObject GameOverScreen;
+    public GameObject PauseScreen;
     public VoidEventchannel onPlayerDeath;
     private void OnEnable()
     {
@@ -16,21 +17,42 @@ public class CurrentScreenManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameOverScreen.SetActive(false);   
+        GameOverScreen.SetActive(false);
+        PauseScreen.SetActive(false);   
     }
 
     private void Die(){
         GameOverScreen.SetActive(true);
     }
+    public void ResumeGame()
+    {
+        PauseScreen.SetActive(false); // Cacher le menu pause
+        Time.timeScale = 1;
+    }
+    public void PauseGame()
+    {
+        PauseScreen.SetActive(true); // Afficher le menu pause
+        Time.timeScale = 0;
+    }
+    public void TogglePause()
+    {
+        if (Time.timeScale == 0)
+        {
+            ResumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+
+
     // Update is called once per frame
     void Update()
     {
+        
         if(Input.GetKeyDown(KeyCode.Escape)){
-            if(Time.timeScale == 0){
-                Time.timeScale = 1;
-            }else{
-                Time.timeScale = 0;
-            }
+            TogglePause();
         }
 #if UNITY_EDITOR  
         if(Input.GetKeyDown(KeyCode.R)){
